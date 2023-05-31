bf9b4b823bfd1081cb1de78777bc339ce031fc4e
AlekseiYuzhanin <zombehalk@gmail.com>
2023-05-31 18:20:29 +0700

    guard auth add

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module,forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import {JwtModule} from '@nestjs/jwt'
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports:[
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || 'SECRET',
       signOptions:{
         expiresIn: '24h'
       }
     })
-  ]
+  ],
+  exports: [JwtModule,AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable,HttpException,HttpStatus } from '@nestjs/common';
+import { Injectable,HttpException,HttpStatus,UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import {JwtService} from '@nestjs/jwt'
@@ -14,7 +14,16 @@ export class AuthService {
     }
 
     async login( userDto:CreateUserDto){
-        
+        const user = await this.validateUser(userDto)
+        return this.generateToken(user)
+    }
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email)
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if (user && passwordEquals){
+            return user;
+        }
+        throw new UnauthorizedException({message: 'Incorrect email or password'})
     }
 
     async registration( userDto:CreateUserDto){
@@ -27,7 +36,7 @@ export class AuthService {
         return this.generateToken(user)
     }
 
-    async generateToken(user: User){
+    private async generateToken(user: User){
         const payload = {email: user.email, id:user.id, roles:user.roles }
         return {
             token: this.jwtService.sign(payload)
